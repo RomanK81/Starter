ab262277f220a7841ce7d59ccbc008dfdbf92556
Roman Kirzner <kirzner.r@gmail.com>
2018-08-19 14:42:46 +0300

    Remove localhost from Clien

--- src/app/views/books/api.service.ts
+++ src/app/views/books/api.service.ts
@@ -6,7 +6,7 @@ import { catchError, tap, map } from 'rxjs/operators';
 const httpOptions = {
   headers: new HttpHeaders({'Content-Type': 'application/json'})
 };
-const apiUrl = "http://localhost:3000/api/books";
+const apiUrl = "/api/books";
 
 @Injectable({
   providedIn: 'root'
